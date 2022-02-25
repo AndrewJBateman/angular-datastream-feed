4a369583fddaaa3b3bd97228dd4cc717a22c2d8d
AndrewJBateman <gomezbateman@yahoo.com>
2022-02-25 18:30:30 +0100

    fix: add title & .gitignore file

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public title = "The Pusher App"
 
   constructor() { }
 
