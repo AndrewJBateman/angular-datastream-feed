cc69148f6c2fc466419b31625cfbe7b6b9bf287d
AndrewJBateman <gomezbateman@yahoo.com>
2022-12-05 14:31:01 +0100

    chore: dependencies updated, rem unrequired code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<reddit-search></reddit-search>`
 })
 export class AppComponent {
   title = 'angular-datastream-feed';
